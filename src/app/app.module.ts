75f687f1e82838374b695d641982054cac14ff5f
hey-now <rubinand22@gmail.com>
2023-02-16 11:39:56 -0800

    creating a books module to serve data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
 import { BookComponent } from './book/book.component';
+import { BooksModule } from './books/books.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BookComponent } from './book/book.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BooksModule
   ],
   providers: [],
   bootstrap: [AppComponent]
