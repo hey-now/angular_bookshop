7cb0be5769ad029753de6aa539f88834344a5bf2
hey-now <rubinand22@gmail.com>
2023-02-16 11:50:41 -0800

    restructuring for books module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BooksComponent } from './books/books.component';
-import { BookComponent } from './book/book.component';
 import { BooksModule } from './books/books.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent,
-    BookComponent
   ],
   imports: [
     BrowserModule,
--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BooksService } from './books.service';
+import { BooksComponent } from './books.component';
+import { BookComponent } from './book/book.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BooksComponent, BookComponent],
   providers: [BooksService],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [BooksComponent]
 })
 export class BooksModule { }
